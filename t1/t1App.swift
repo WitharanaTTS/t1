080cfac3f49e377aa917dbe8b3c793fa6444bd58
LaknaSandeepaniAttigala <92353904+LaknaSandeepani@users.noreply.github.com>
2023-09-13 20:43:20 +0530

    Initial Commit

---
+++ t1/t1App.swift
@@ -0,0 +1,17 @@
+//
+//  t1App.swift
+//  t1
+//
+//  Created by Lakna Attigala on 2023-09-13.
+//
+
+import SwiftUI
+
+@main
+struct t1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
